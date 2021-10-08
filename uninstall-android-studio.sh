bc53f40104f2e02721678498a2abb3556b83b947
rwx788 <rodion.iafarov@jetbrains.com>
2021-10-08 18:27:55 +0200

    Add script to uninstall android studio including SDKs

---
+++ uninstall-android-studio.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -Rf /Applications/Android\ Studio.app
+rm -Rf ~/Library/Preferences/AndroidStudio*
+rm -Rf ~/Library/Preferences/com.google.android.*
+rm -Rf ~/Library/Preferences/com.android.*
+rm -Rf ~/Library/Application\ Support/AndroidStudio*
+rm -Rf ~/Library/Logs/AndroidStudio*
+rm -Rf ~/Library/Caches/AndroidStudio*
+rm -Rf ~/.AndroidStudio*
+rm -Rf ~/.gradle
+rm -Rf ~/.android
+rm -Rf ~/Library/Android*
+rm -Rf /usr/local/var/lib/android-sdk/
