3ee3fd2f870f832c68a302d911e4614dd928a0d9
rwx788 <>
2025-12-17 19:56:31 +0100

    Add script to configure terminal on macOS

---
+++ setup_term_macos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setup brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /Users/rwx788/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/rwx788/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install tools for gpg agent configuration
+brew install oh-my-zsh
+brew install tmux
+brew install pinentry
+brew install gpg
+brew install vim
+brew install wget
+brew install iproute2mac
+
+# Install powerlevel theme for zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
